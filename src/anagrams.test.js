23c6220bff3c314faa671394cedea80f59b4daf9
Zvonimir Perkovic <zperkovi@gmail.com>
2021-09-20 15:29:54 -0400

    Anagram practice tests.

---
+++ src/anagrams.test.js
@@ -0,0 +1,24 @@
+import { expect } from 'chai';
+import { isAnagram } from './anagrams';
+
+describe('isAnagram - basic functionality', () => {
+
+  // are anagrams
+  // ex. 'elbow' 'below'
+  it('return true when two known anagrams are passed in', () => {
+    const expected = true;
+    const actual = isAnagram('listen', 'silent');
+    expect(actual).to.equal(expected);
+  });
+
+  // not anagrams
+  // ex. 'elbows' 'below'
+  it('returns false when either of the strings has extra letters', () => {
+    const expected = false;
+    const actual = isAnagram('elbows', 'below');
+    expect(actual).to.equal(expected);
+
+    const actual2 = isAnagram('below', 'elbows');
+    expect(actual2).to.equal(expected);
+  });
+});
